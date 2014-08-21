f7a9f45b6b772593732b45333329d1d129dd39c5
henryhoang <henry.hoang@j2.com>
2014-08-21 15:08:32 -0700

    Create date.sh

---
+++ date.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script displays the date, time, username and
+   # current directory.
+clear
+   echo "Date and time is:"
+   date
+   echo
+   echo "Your username is: `whoami` \n"
+   echo "Your current directory is: \c"
+   pwd
